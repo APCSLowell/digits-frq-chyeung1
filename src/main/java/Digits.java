fc5595b4caf20792f1a57f68aac45bb680675fa0
chyeung1 <143123817+chyeung1@users.noreply.github.com>
2024-04-14 22:30:59 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -7,13 +7,22 @@ public class Digits
 
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
-	    
+	   String bruh = num.toString();
+	   for(int i = 0; i < bruh.length; i++){
+		digitList.add(bruh.chartAt(i));
+	   }
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		
-
+	 int comp = 0;
+	for(int i = 0; i < digitList.size(); i++){
+	if(comp < digitList.get(i))
+	comp = digitList.get(i);
+	else if (comp >= digitList.get(i))
+	return false;
+	}
+	return true;
 	}
 	
 	public String toString()
